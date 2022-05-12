8caf747de113dae4fa421be4fd8bd6d213341eaf
ithirteeng <ithirteengw@gmail.com>
2022-05-12 14:12:29 +0700

    strange

--- app/src/main/java/com/example/codeblocksproject/model/EndProgramBlock.kt
+++ app/src/main/java/com/example/codeblocksproject/model/EndProgramBlock.kt
@@ -3,12 +3,13 @@ package com.example.codeblocksproject.model
 import android.annotation.SuppressLint
 import android.content.Context
 import android.util.AttributeSet
+import android.view.LayoutInflater
 import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.codeblocksproject.R
 
 @SuppressLint("ViewConstructor")
 class EndProgramBlock @JvmOverloads constructor(
-    view: View,
     context: Context,
     attrs: AttributeSet? = null
 ) : CustomView, ConstraintLayout(context, attrs) {
@@ -16,8 +17,7 @@ class EndProgramBlock @JvmOverloads constructor(
     override var previousId: Int = 0
     override var nextId: Int = -1
     override val blockType = BlockTypes.END_PROGRAM_BLOCK_TYPE
-    override val blockView: View = view
-    //TODO: change to end program layout
+    override val blockView : View = LayoutInflater.from(context).inflate(R.layout.start_program_block, this)
 
     override var position = 0
     override val pattern = "}"
@@ -26,6 +26,5 @@ class EndProgramBlock @JvmOverloads constructor(
         return pattern
     }
     override fun makeEditTextsDisabled() {
-        TODO("Not yet implemented")
     }
 }
--- app/src/main/java/com/example/codeblocksproject/model/StartProgramBlock.kt
+++ app/src/main/java/com/example/codeblocksproject/model/StartProgramBlock.kt
@@ -3,12 +3,13 @@ package com.example.codeblocksproject.model
 import android.annotation.SuppressLint
 import android.content.Context
 import android.util.AttributeSet
+import android.view.LayoutInflater
 import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.codeblocksproject.R
 
 @SuppressLint("ViewConstructor")
 class StartProgramBlock @JvmOverloads constructor(
-    view: View,
     context: Context,
     attrs: AttributeSet? = null
 ) : CustomView, ConstraintLayout(context, attrs) {
@@ -16,7 +17,7 @@ class StartProgramBlock @JvmOverloads constructor(
     override var previousId = -1
     override var nextId = 0
     override val blockType = BlockTypes.START_PROGRAM_BLOCK_TYPE
-    override val blockView = view
+    override val blockView : View = LayoutInflater.from(context).inflate(R.layout.start_program_block, this)
 
     override val pattern = "{"
     override var position=0
@@ -25,6 +26,5 @@ class StartProgramBlock @JvmOverloads constructor(
         return pattern
     }
     override fun makeEditTextsDisabled() {
-        TODO("Not yet implemented")
     }
 }
